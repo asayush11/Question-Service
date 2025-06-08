84afdf154c17ea6de7c7cc2083e29d098c57d5b1
Ayush Sharma <asayush35@gmail.com>
2025-06-08 09:47:21 +0530

    Adding no-arg all-arg constructors

--- src/main/java/com/example/questions_service/DTO/QuestionDTO.java
+++ src/main/java/com/example/questions_service/DTO/QuestionDTO.java
@@ -26,6 +26,20 @@ public class QuestionDTO {
 
     private String solution;
 
+    public QuestionDTO() {
+    }
+
+    public QuestionDTO(String question, String category, String difficulty, String option1, String option2, String option3, String option4, String solution) {
+        this.question = question;
+        this.category = category;
+        this.difficulty = difficulty;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+        this.solution = solution;
+    }
+
     public String getQuestion() {
         return question;
     }
--- src/main/java/com/example/questions_service/Entity/Question.java
+++ src/main/java/com/example/questions_service/Entity/Question.java
@@ -19,6 +19,21 @@ public class Question {
     private String option4;
     private String solution;
 
+    public Question() {
+    }
+
+    public Question(Integer questionId, String question, String category, String difficulty, String option1, String option2, String option3, String option4, String solution) {
+        this.questionId = questionId;
+        this.question = question;
+        this.category = category;
+        this.difficulty = difficulty;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+        this.solution = solution;
+    }
+
     public Question(QuestionDTO questionDTO) {
         this.question = questionDTO.getQuestion();
         this.category = questionDTO.getCategory();
