8a4d28e36a50cda375360668cab17dbbb513ec2f
Ayush Sharma <asayush35@gmail.com>
2025-06-16 19:38:39 +0530

    No filter on Options, let DB sleep

--- src/main/java/com/example/questions_service/Utility/JWTAuthFilter.java
+++ src/main/java/com/example/questions_service/Utility/JWTAuthFilter.java
@@ -9,6 +9,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import jakarta.servlet.Filter;
 
 @Component
@@ -28,7 +31,8 @@ public class JWTAuthFilter implements Filter {
         String path = request.getRequestURI();
 
         // Allow public endpoints
-        if (path.startsWith("/users")) {
+        List<String> publicEndpoints = Arrays.asList("/users", "/actuator");
+        if (publicEndpoints.stream().anyMatch(path::startsWith) || request.getMethod().equalsIgnoreCase("OPTIONS")) {
             chain.doFilter(req, res);
             return;
         }
